837ee12fdaa44905b267d558ebcd3093fdcf8b84
Lukas Van Orshoven <vanorshovenlukas@gmail.com>
2020-03-29 20:27:08 +0200

    Another try (netlify-lambda now normal dependency)

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ShadowIcon from './ShadowIcon';
 import logo from "../img/logo.svg"
+import { Link } from 'react-router-dom';
 
 const Logo = () => {
 
 
-  return <div className="logo">
+  return <Link to="/" className="logo">
     <ShadowIcon style={{ marginBottom: '.2em' }}>
       <img src={logo} alt="Sendavoice logo" />
     </ShadowIcon>
     <span>Sendavoice</span>
-  </div>
+  </Link>
 };
 
 export default Logo;
